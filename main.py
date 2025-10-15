ca239cb8bc4bf0e10ce36d961fed35ff6f7090c1
Shayla Sexton <shaylae116@gmail.com>
2025-10-14 21:59:10 -0400

    Update comments and ideas in main

--- main.py
+++ main.py
@@ -30,4 +30,7 @@ if __name__ == '__main__':
     # 4. Keep track of total profit from owned properties
     # May make some github project issues to track these!
 
+    # will also need to be definityive on how the total profit will be. maybe a mode to start from price put in, or pure profits. Or Both!
+    # will also need to deciede will the method have barriers for over buying and over selling properites? or will buttons be disabled. need to exeperiment with QtPy
+
 
