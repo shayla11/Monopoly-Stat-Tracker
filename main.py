2c6bed4870fd5297c6ec7902e49a2c685044ceb0
Shayla Sexton <shaylae116@gmail.com>
2025-10-19 22:38:14 -0400

    Get hotel and house buttons working properly
    fix bugs so that house max out at 4 and current rent is properly set for houses and hotel toggle

--- main.py
+++ main.py
@@ -49,25 +49,57 @@ class MainWindow(QMainWindow):
         # If property already owned, skip adding a widget
         if board_property.value.owned:
             print(board_property.name + " already own!")
-            return
-
-        # Else proceed with adding the property to the own list and adding a new widget box
         else:
+            # Else proceed with adding the property to the own list and adding a new widget box
             board_property.value.owned = True
             self.owned_properties.append(board_property.name)
             print("Owned Properties" + str(self.owned_properties))
-            box = PropertyBox(property_name)
+            box = PropertyBox(property_name, board_property)
             box.setFixedSize(250, 400)
             self.hbox_layout.addWidget(box)
 
 
 class PropertyBox(QWidget):
-    def __init__(self, name):
+    def __init__(self, name: str, board_property: Utilities | Railroads | Properties):
         super().__init__()
         uic.loadUi("propertyCard.ui", self)
 
+        # The actual Property object
+        self.property_enum = board_property
+        self.property = board_property.value  # <--- Access the Property instance
+
         # Set the name from the box to the property
         self.propertyNameLabel.setText(name)
+
+        # Connect signals
+        self.hotelOwnedCheckBox.setChecked(False)
+        self.housesOwnedSpinBox.valueChanged.connect(self.update_rent)
+        self.hotelOwnedCheckBox.clicked.connect(self.toggle_hotel)
+
+        # Initial rent
+        self.update_rent()
+
+    def toggle_hotel(self):
+        self.hotelOwnedCheckBox = not self.hotelOwnedCheckBox
+
+        # Reset houses if hotel is turned on
+        if self.hotelOwnedCheckBox:
+            self.housesOwnedSpinBox.setValue(0)
+
+        self.update_rent()
+
+    def update_rent(self):
+        if not self.hotelOwnedCheckBox:
+            rent = self.property.property_values[5]  # Hotel rent
+        else:
+            house_count = self.housesOwnedSpinBox.value()
+            if house_count > 4:
+                print("Max of 4 houses reached!")
+                self.housesOwnedSpinBox.setValue(4)
+                house_count = 4
+            rent = self.property.property_values[house_count]
+
+        self.currentRentAmountlabel.setText(f"${rent}")
 
 
 if __name__ == "__main__":
